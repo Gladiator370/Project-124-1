2cef9a55551be635ca0e56e2806d4ef2ccad7355
Gladiator370 <75198510+Gladiator370@users.noreply.github.com>
2021-11-10 17:17:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup() 
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(400, 400);
+    canvas.position(600, 100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() 
+{
+    console.log("model is Inatialized");
+}
+
+function gotPoses(results) 
+{
+    if (results.length > 0) 
+    {
+        console.log(results);
+    }
+}
+
+function draw() 
+{
+    background("#809aed");
+}
